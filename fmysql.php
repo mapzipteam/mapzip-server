9ac07360d0f4fadfa21d8e28dbe7201aac2dbbfd
SSUHan <joyangel93@naver.com>
2015-07-25 20:26:08 +0900

    기본 mysql 연결 인클루드 파일이다

---
+++ fmysql.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+function connect_mysqli($ip,$user,$password,$db){
+	if(!$conn = mysqli_connect($ip,$user,$password)){
+		echo "mysql 연결실패<br />";
+	}
+	if(!mysqli_select_db($conn,$db)){
+		echo "db 선택 실패<br />";
+	}
+	return $conn;
+}
+
+
+
+?>

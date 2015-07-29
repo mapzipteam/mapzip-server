945269eb97e5f19015d381024c358bf096498660
SSUHan <joyangel93@naver.com>
2015-07-29 14:30:57 +0900

    회원가입정보 넘어오면 데이터베이스에 넣고, 중복체크까지 완료

---
+++ login/joincheck.php
@@ -0,0 +1,51 @@
+<?php
+include("fmysql.php");
+include("mapzip-login-define.php");
+$user_id = $_POST['userid'];
+$user_pw = $_POST['userpw'];
+$user_name = $_POST['username'];
+echo "id:".$user_id." pw:".$user_pw." name:".$username." from server\n";
+
+if(!$conn = connect_mysqli(MYSQL_IP,MAIN_DB,DB_PASSWORD,USE_DB)){
+	echo "connnection error!\n";
+}
+else{
+echo "connect success!\n";
+}
+$sql = "SELECT * FROM ".USER_TABLE;
+
+if(!$result = mysqli_query($conn,$sql)){
+	echo "query fail...\n";
+}
+
+if(joinOK($user_id,$result)){
+	$sql = "INSERT INTO ".USER_TABLE." (userid, username, userpw, created) VALUES ('".$user_id."','".$user_name."','".$user_pw."',now())";
+	if(mysqli_query($conn,$sql)){
+		echo("insert success!\n");
+
+	}
+	else{
+		echo "insert fail...\n";
+	}
+	
+
+}
+else{
+	echo("That id already exists in server\n");
+}
+
+
+
+
+
+function joinOK($id,$result){
+	while($row = mysqli_fetch_assoc($result)){
+		if(strcmp($id,$row['userid'])==0){
+			
+			return false;
+		}
+	}
+	return true;
+}
+
+?>
---
+++ login/mapzip-login-define.php
@@ -0,0 +1,11 @@
+<?php
+//path
+define("JP_PATH",'http://ljs93kr.cafe24.com/JPPage/');
+
+// mysql
+define("MYSQL_IP","localhost");
+define("MAIN_DB","ljs93kr");
+define("DB_PASSWORD","wnddnjsQkd");
+define("USE_DB","ljs93kr");
+define("USER_TABLE","mz_user_info");
+?>

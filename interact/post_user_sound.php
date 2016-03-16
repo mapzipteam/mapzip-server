2c4bdcad8e8121cbb5c5af363bcfb561f999c3e9
이준수 <ljs93kr@gmail.com>
2016-03-16 17:07:16 +0900

    post user sound 버전관리 코드 추가

--- interact/post_user_sound.php
+++ interact/post_user_sound.php
@@ -15,29 +15,40 @@ if(($value['build_version'] >= BUILD_VERSION_GARNET) && ($value['build_version']
 	}
 	else{
 		// db connection success
-	}
-}else{
+		$sql = "INSERT INTO ".USER_SOUND_TABLE." values(null, '{$value['userid']}', '{$value['username']}', '{$value['category']}', '{$value['contents']}', 0, now());";
 
-}
-
-if(!$conn = connect_mysqli(MYSQL_IP,MAIN_DB,DB_PASSWORD,USE_DB)){
-	//echo "connnection error!\n";
-	$to_client['state']=DB_CONNECTION_ERROR;
-}
-else{
-//echo "connect success!\n";
-}
-
-
-$sql = "INSERT INTO ".USER_SOUND_TABLE." values(null, '{$value['userid']}', '{$value['username']}', '{$value['category']}', '{$value['contents']}', 0, now());";
-
-if(!mysqli_query($conn,$sql)){
+		if(!mysqli_query($conn,$sql)){
 	// insert fail
-	$to_client['state'] = USER_SOUND_INSERT_FAIL;
+			$to_client->setFields('state', USER_SOUND_INSERT_FAIL);
+			$to_client->setDebugs('insert user sound table fail', $sql);
+		}else{
+			$to_client->setFields('state', USER_SOUND_INSERT_SUCCESS);
+		}
+	}
+	echo json_encode($to_client->build());
 }else{
-	$to_client['state'] = USER_SOUND_INSERT_SUCCESS;
+	if(!$conn = connect_mysqli(MYSQL_IP,MAIN_DB,DB_PASSWORD,USE_DB)){
+	//echo "connnection error!\n";
+		$to_client['state']=DB_CONNECTION_ERROR;
+	}
+	else{
+//echo "connect success!\n";
+	}
+
+
+	$sql = "INSERT INTO ".USER_SOUND_TABLE." values(null, '{$value['userid']}', '{$value['username']}', '{$value['category']}', '{$value['contents']}', 0, now());";
+
+	if(!mysqli_query($conn,$sql)){
+	// insert fail
+		$to_client['state'] = USER_SOUND_INSERT_FAIL;
+	}else{
+		$to_client['state'] = USER_SOUND_INSERT_SUCCESS;
+	}
+
+	echo json_encode($to_client);
+
 }
 
-echo json_encode($to_client);
+
 
 ?>
